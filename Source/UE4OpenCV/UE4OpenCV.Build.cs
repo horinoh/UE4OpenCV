b833e52535dc32622b8a54cd523c13849b2c5148
horinoh <development@horino.sakura.ne.jp>
2016-06-06 01:14:01 +0900

    4.12対応

--- Source/UE4OpenCV/UE4OpenCV.Build.cs
+++ Source/UE4OpenCV/UE4OpenCV.Build.cs
@@ -27,7 +27,10 @@ public class UE4OpenCV : ModuleRules
 
 		if(UnrealTargetPlatform.Win64 == Target.Platform)
 		{
-			var ModulePath = Path.GetDirectoryName(RulesCompiler.GetModuleFilename(GetType().Name));
+			FileReference FR;
+            UProjectInfo.TryGetProjectForTarget("UE4OpenCV", out FR);
+            RulesAssembly RA = RulesCompiler.CreateProjectRulesAssembly(FR);
+            var ModulePath = Path.GetDirectoryName(RA.GetModuleFileName(GetType().Name).CanonicalName);
 			var ThirdPartyPath = Path.GetFullPath(Path.Combine(ModulePath, "..", "..", "ThirdParty"));
 			var BasePath = Path.Combine(ThirdPartyPath, "opencv", "build");
 
